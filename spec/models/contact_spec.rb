03c2289a7602592c4ddc258cf85250e2c0c1126a
togana <o.togana@gmail.com>
2017-10-15 21:19:19 +0900

    性と名とメールがあるか確認

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 describe Contact do
   # 姓と名とメールがあれば有効な状態であること
-  it "is valid with a firstname, lastname and email"
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+
   # 名がなければ無効な状態であること
   it "is invalid without a firstname"
   # 姓がなければ無効な状態であること
