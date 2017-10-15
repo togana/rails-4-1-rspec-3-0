43deb96112a98101755eec47fcc84711134f577e
togana <o.togana@gmail.com>
2017-10-15 23:01:45 +0900

    メールアドレスは重複しない

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -31,7 +31,19 @@ describe Contact do
   end
 
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+      firstname: 'Joe', lastname: 'Tester',
+        email: 'tester@example.com'
+      )
+      contact = Contact.new(
+        firstname: 'Jane', lastname: 'Tester',
+        email: 'tester@example.com'
+      )
+      contact.valid?
+      expect(contact.errors[:email]).to include("has already been taken")
+  end
+
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string"
 end
