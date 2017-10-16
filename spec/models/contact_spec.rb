004231cfe8bba23843e3e2c46ac80a42ea54946a
togana <o.togana@gmail.com>
2017-10-16 12:40:48 +0900

    かんたんによびだしデキルように設定したfixturesは使わないので殺した

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,7 +3,7 @@ describe Contact do
 
   # 有効なファクトリを持つこと
   it "has a valid factory" do
-    expect(FactoryGirl.build(:contact)).to be_valid
+    expect(build(:contact)).to be_valid
   end
 
   # 姓と名とメールがあれば有効な状態であること
@@ -17,36 +17,36 @@ describe Contact do
 
   # 名がなければ無効な状態であること
   it "is invalid without a firstname" do
-    contact = FactoryGirl.build(:contact, firstname: nil)
+    contact = build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   # 姓がなければ無効な状態であること
   it "is invalid without a lastname" do
-    contact = FactoryGirl.build(:contact, lastname: nil)
+    contact = build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    contact = FactoryGirl.build(:contact, email: nil)
+    contact = build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
-    FactoryGirl.create(:contact, email: 'aaron@example.com')
-    contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
+    create(:contact, email: 'aaron@example.com')
+    contact = build(:contact, email: 'aaron@example.com')
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string" do
-    contact = contact = FactoryGirl.build(:contact,
+    contact = contact = build(:contact,
       firstname: 'John',
       lastname: 'Doe')
     expect(contact.name).to eq 'John Doe'
