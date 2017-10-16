7639693fb22a2336bf6a7bcab79333fadfa39498
togana <o.togana@gmail.com>
2017-10-16 13:53:23 +0900

    タイプを継承デキルようにしてテストにファクトリを適応

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,17 +2,13 @@ require 'rails_helper'
 describe Phone do
   # 連絡先ごとに重複した電話番号を許可しないこと
   it "does not allow duplicate phone numbers per contact" do
-    contact = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com'
-    )
-    contact.phones.create(
-      phone_type: 'home',
+    contact = create(:contact)
+    create(:home_phone,
+      contact: contact,
       phone: '785-555-1234'
     )
-    mobile_phone = contact.phones.build(
-      phone_type: 'mobile',
+    mobile_phone = build(:mobile_phone,
+      contact: contact,
       phone: '785-555-1234'
     )
 
@@ -22,11 +18,7 @@ describe Phone do
 
   # 2件の連絡先で同じ電話番号を共有できること
   it "allows two contacts to share a phone number" do
-    create(:phone,
-      phone_type: 'home',
-      phone: "785-555-1234")
-    expect(build(:phone,
-      phone_type: 'home',
-      phone: "785-555-1234")).to be_valid
+    create(:home_phone, phone: "785-555-1234")
+    expect(build(:home_phone, phone: "785-555-1234")).to be_valid
   end
 end
