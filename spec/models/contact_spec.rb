4bfe6541206f6e871ca2b0c986f56aee9452c79c
togana <o.togana@gmail.com>
2017-10-16 11:26:01 +0900

    matchしなかったものは結果に存在しない事を確認した

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -70,4 +70,24 @@ describe Contact do
     )
     expect(Contact.by_letter("J")).to eq [johnson, jones]
   end
+
+  # マッチしなかったものは結果に含まれないこと
+  it "omits results that do not match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@example.com'
+    )
+    jones = Contact.create(
+      firstname: 'Tim',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjohnson@example.com'
+    )
+    expect(Contact.by_letter("J")).not_to include smith
+  end
 end
