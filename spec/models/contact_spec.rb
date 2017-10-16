cbd5d935961459b726f638804d4595503acb16a6
togana <o.togana@gmail.com>
2017-10-16 11:31:28 +0900

    describe と contextを利用して切り分けた

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -51,43 +51,52 @@ describe Contact do
     expect(contact.name).to eq 'John Doe'
   end
 
-  # マッチした結果をソート済みの配列として返すこと
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-    expect(Contact.by_letter("J")).to eq [johnson, jones]
-  end
+  # 文字で姓をフィルタする
+  describe "filter last name by letter" do
+  # マッチする文字の場合
+    context "matching letters" do
+      # マッチした結果をソート済みの配列として返すこと
+      it "returns a sorted array of results that match" do
+        smith = Contact.create(
+          firstname: 'John',
+          lastname: 'Smith',
+          email: 'jsmith@example.com'
+        )
+        jones = Contact.create(
+          firstname: 'Tim',
+          lastname: 'Jones',
+          email: 'tjones@example.com'
+        )
+        johnson = Contact.create(
+          firstname: 'John',
+          lastname: 'Johnson',
+          email: 'jjohnson@example.com'
+        )
+        expect(Contact.by_letter("J")).to eq [johnson, jones]
+      end
+    end
 
-  # マッチしなかったものは結果に含まれないこと
-  it "omits results that do not match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-    expect(Contact.by_letter("J")).not_to include smith
+    # マッチしない文字の場合
+    context "non-matching letters" do
+      # マッチしなかったものは結果に含まれないこと
+      it "omits results that do not match" do
+        smith = Contact.create(
+          firstname: 'John',
+          lastname: 'Smith',
+          email: 'jsmith@example.com'
+        )
+        jones = Contact.create(
+          firstname: 'Tim',
+          lastname: 'Jones',
+          email: 'tjones@example.com'
+        )
+        johnson = Contact.create(
+          firstname: 'John',
+          lastname: 'Johnson',
+          email: 'jjohnson@example.com'
+        )
+        expect(Contact.by_letter("J")).not_to include smith
+      end
+    end
   end
 end
